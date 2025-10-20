a81da71a5ba91a8fcdcada0adad38b9f30a90a7c
Miles Punch <71351127+miles-p@users.noreply.github.com>
2025-10-20 17:23:47 +1100

    Add and test FastLED

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include <Arduino.h>
+#include <FastLED.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int LED_PIN = 12;
+const int NUM_LEDS = 300;
+
+CRGB leds[NUM_LEDS];
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // put your setup code here, to run once:
+    FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
+
+    for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = CRGB::Red;
+    }
+    FastLED.show();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    // put your main code here, to run repeatedly:
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
