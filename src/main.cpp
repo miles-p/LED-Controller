1590d7b177b7388c36a046f72fe4686c3499e871
Miles Punch <71351127+miles-p@users.noreply.github.com>
2025-11-01 13:34:26 +1100

    working artnet

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,47 @@
-// libraries
-#include <Arduino.h>
+#include <SPI.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
 #include <FastLED.h>
-#include <ArtnetEther.h>
 
-// instance objects
-ArtnetEtherReceiver artnetReceiver;
+#define LED_PIN     6
+#define NUM_LEDS    300
+#define ARTNET_PORT 6454
 
-// constants
-const int LED_PIN = 6;
-const int NUM_LEDS = 300;
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+IPAddress ip(192, 168, 1, 50);
 
-const int START_ADDRESS = 0; // DMX start address
-const int UNIVERSE = 0;
-
-const IPAddress IP(192, 168, 1, 50); // Set your desired static IP
-const byte MAC[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB}; // Set your desired MAC address
-
-// global variables
+EthernetUDP Udp;
 CRGB leds[NUM_LEDS];
 
-// callback functions
-void callback(const uint8_t *data, uint16_t size, const ArtDmxMetadata &metadata, const ArtNetRemoteInfo &remote) {
-    // you can also use pre-defined callbacks
-
-}
-
 void setup() {
-    // LED configuration!
-    FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
-    FastLED.clear();
-    FastLED.show();
-
-    Ethernet.begin(MAC, IP);
-    delay(500); // Give the Ethernet shield a second to initialize
-    artnetReceiver.begin();
-    artnetReceiver.subscribeArtDmxUniverse(UNIVERSE, callback);
+  Ethernet.begin(mac, ip);
+  Udp.begin(ARTNET_PORT);
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.clear();
+  FastLED.show();
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    artnetReceiver.parse();
+  uint8_t packet[530];
+  int len = Udp.parsePacket();
+  if (len > 0) {
+    Udp.read(packet, 530);
+
+    // verify Art-Net and OpCode = ArtDMX (0x5000)
+    if (memcmp(packet, "Art-Net", 7) == 0 && packet[8] == 0x00 && packet[9] == 0x50) {
+      // length is in bytes 16 (high) and 17 (low), data starts at byte 18
+      uint16_t dmxLen = (packet[16] << 8) | packet[17];
+      uint16_t start = 18;
+
+      // clamp to actual received UDP length (len from Udp.parsePacket()) to avoid overruns
+      if (dmxLen > (uint16_t)(len - start)) dmxLen = (uint16_t)(len - start);
+
+      for (uint16_t i = 0; i + 2 < dmxLen && i / 3 < NUM_LEDS; i += 3) {
+        leds[i / 3].r = packet[start + i];
+        leds[i / 3].g = packet[start + i + 1];
+        leds[i / 3].b = packet[start + i + 2];
+      }
+      FastLED.show();
+    }
+  }
 }
