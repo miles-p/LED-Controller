5ecb2ddc1747b33e9c53d77287aeba4c9b83bf34
Miles Punch <71351127+miles-p@users.noreply.github.com>
2025-11-14 12:51:42 +1100

    getting somewhere

--- src/main.cpp
+++ src/main.cpp
@@ -10,52 +10,22 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
-#include <ArtnetEther.h>
+#include <E131.h>
 #include <FastLED.h>
 
 // Defined constants
 #define LED_PIN     6
 #define NUM_LEDS    300
-#define ARTNET_PORT 6454
 #define START_UNIVERSE 0 // we may not want to begin on universe 0 (remember that artnet is 0-indexed)
 
 // Static IP and MAC address
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 1, 50); // Set your desired static IP here
-ArtnetEtherReceiver artnet;
+E131 e131;
 
 /// @brief Array to hold the LED data
 CRGB leds[NUM_LEDS];
 
-void artnet_callback(const uint8_t *data, uint16_t size, const ArtDmxMetadata &metadata, const ArtNetRemoteInfo &remote) {
-  if (DEBUG) {
-    Serial.print("Received ArtDmx packet: Universe ");
-    Serial.print(metadata.universe);
-    Serial.print(", Size ");
-    Serial.print(size);
-    Serial.print(", [");
-    Serial.print(data[0]);
-    Serial.print(", ");
-    Serial.print(data[1]);
-    Serial.print(", ");
-    Serial.print(data[2]);
-    Serial.println("...]");
-  }
-  // Calculate the starting LED index for this universe
-  int universeOffset = (metadata.universe - START_UNIVERSE) * 170;
-  
-  // Update LEDs for this universe (170 LEDs per universe, 3 bytes per LED)
-  int ledsInThisPacket = min(size / 3, 170);
-  for (int i = 0; i < ledsInThisPacket; i++) {
-    int ledIndex = universeOffset + i;
-    if (ledIndex < NUM_LEDS) {
-      leds[ledIndex].r = data[i * 3 + 0];
-      leds[ledIndex].g = data[i * 3 + 1];
-      leds[ledIndex].b = data[i * 3 + 2];
-    }
-  }
-  FastLED.show();
-}
 
 void init_leds() {
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
@@ -64,10 +34,20 @@ void init_leds() {
 }
 
 void init_networking() {
-  Ethernet.begin(mac, ip);
-  delay(100); // Give the Ethernet shield a second to initialize
-  artnet.begin(ARTNET_PORT);
-  artnet.subscribeArtDmx(artnet_callback);
+  e131.begin(mac);
+}
+
+void write_leds(int num_channels) {
+  // Copy E1.31 data to LED array
+  for (int i = 0; i < NUM_LEDS; i++) {
+    int channel_index = i * 3;
+    if (channel_index + 2 < num_channels) {
+      leds[i].r = e131.data[channel_index];
+      leds[i].g = e131.data[channel_index + 1];
+      leds[i].b = e131.data[channel_index + 2];
+    }
+  }
+  FastLED.show();
 }
 
 void setup() {
@@ -80,5 +60,21 @@ void setup() {
 }
 
 void loop() {
-  artnet.parse();
+  // Check for incoming E1.31 packets
+  uint16_t num_channels = e131.parsePacket();
+  if (num_channels != 0) {
+    if (DEBUG) {
+      Serial.print(F("Universe "));
+      Serial.print(e131.universe);
+      Serial.print(F(" / "));
+      Serial.print(num_channels);
+      Serial.print(F(" Channels | Packet#: "));
+      Serial.print(e131.stats.num_packets);
+      Serial.print(F(" / Errors: "));
+      Serial.print(e131.stats.packet_errors);
+      Serial.print(F(" / CH1: "));
+      Serial.println(e131.data[0]);
+    }
+    write_leds(num_channels);
+  }
 }
